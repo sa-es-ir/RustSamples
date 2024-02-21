088413b82ff0553402148d26c417b7a53c85eb8f
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-02-21 23:17:40 +0700

    using shadow and const vars

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,8 +1,20 @@
+const THREE_HOURS_IN_SEC: u32 = 60 * 60 * 3;
 fn main() {
-    let x = 5;
+    let mut x = 5;
     println!("the value of x is {x}");
 
-    x = 6;
+    x += 6;
 
     println!("new value of x is : {x}");
+
+    println!("const value of x is : {THREE_HOURS_IN_SEC}");
+
+    let x = x * 2;
+
+    {
+        let x = x * 2;
+        println!("local scope of x: {x}");
+    }
+
+    println!("outer scope of x : {x}");
 }
