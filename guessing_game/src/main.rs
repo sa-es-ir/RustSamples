23b7dc694c88132dd2298c4bfd472916700b7fce
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-02-28 23:50:58 +0700

    user compare as std::cmp

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,11 @@
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-
-    println!("secret number is {secret_number}");
+    let secret_number = rand::thread_rng().gen_range(1..=100).to_string();
 
     println!("Please input your guess:");
 
@@ -17,4 +16,12 @@ fn main() {
         .expect("Failed to read the line!");
 
     println!("Your guessed number is {guess}");
+
+    let compare_result = match guess.cmp(&secret_number) {
+        Ordering::Equal => "Equal",
+        Ordering::Greater => "Greater",
+        Ordering::Less => "Less",
+    };
+
+    println!("Result is {compare_result}");
 }
