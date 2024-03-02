d43d2087e03235b5c6880c31e03d0a78609b9815
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-03-02 22:31:58 +0700

    add mutable reference as function param

--- references_borrowing/src/main.rs
+++ references_borrowing/src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s1 = String::from("hello");
+
+    let len = calculate_length(&s1);
+
+    println!("string value {} and len {}", s1, len);
+
+    change(&mut s1);
+
+    println!("new value after change string is {}", s1);
+}
+
+fn calculate_length(s: &String) -> usize {
+    s.len()
+}
+
+fn change(s: &mut String) {
+    s.push_str(" world!");
 }
