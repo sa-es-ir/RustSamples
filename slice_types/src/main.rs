66839cb6bad1a39dc8ebecd759808cc4df434214
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-03-02 23:27:10 +0700

    find first word with slice

--- slice_types/src/main.rs
+++ slice_types/src/main.rs
@@ -1,5 +1,28 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello world");
 
-    let s = "fad";
+    let word = first_word(&s);
+
+    let (part1, part2) = s.split_at(word);
+
+    println!("{} --- {}", part1, part2);
+
+    //using string slice
+    let hello = &s[0..word]; // same as &s[..word]
+
+    let world = &s[word..]; // same as &s[word..s.len()]
+
+    println!("using string slice {} -- {}", hello, world);
+}
+
+fn first_word(s: &String) -> usize {
+    let s_bytes = s.as_bytes();
+
+    for (i, &item) in s_bytes.iter().enumerate() {
+        if item == b' ' {
+            return i;
+        }
+    }
+
+    s.len()
 }
