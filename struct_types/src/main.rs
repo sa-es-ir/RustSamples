44bdff928d6589ae6491294c10b47694f62f089c
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-03-04 23:01:23 +0700

    play struct types

---
+++ struct_types/src/main.rs
@@ -0,0 +1,43 @@
+fn main() {
+    let mut user1 = User {
+        active: true,
+        username: String::from("saeed"),
+        email: String::from("mock@email.com"),
+        sign_in_count: 10,
+    };
+
+    println!("old username: {}", user1.username);
+
+    user1.username = String::from("new_saeed");
+    println!("new username: {}", user1.username);
+
+    let new_user = build_user(String::new(), String::from("from_build@mail.com"));
+
+    println!("username: {} email: {}", new_user.username, new_user.email);
+
+    let user2 = User {
+        email: String::from("new_email2@mail.com"),
+        ..user1
+    };
+
+    println!(
+        "user2 --> username: {} email: {}",
+        user2.username, user2.email
+    );
+}
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(username: String, email: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
