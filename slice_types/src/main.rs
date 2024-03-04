f5960c6bc450322fb4f2c1704d0b7fa64b36500d
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-03-04 22:19:05 +0700

    revise slice type on first word

--- slice_types/src/main.rs
+++ slice_types/src/main.rs
@@ -1,7 +1,7 @@
 fn main() {
     let s = String::from("Hello world");
 
-    let word = first_word(&s);
+    let word = first_word_no_slice(&s);
 
     let (part1, part2) = s.split_at(word);
 
@@ -13,9 +13,14 @@ fn main() {
     let world = &s[word..]; // same as &s[word..s.len()]
 
     println!("using string slice {} -- {}", hello, world);
+
+    let s = String::from("singleword");
+    let first_word = first_word_with_slice(&s);
+
+    println!("Got first word with slice: {}", first_word);
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word_no_slice(s: &String) -> usize {
     let s_bytes = s.as_bytes();
 
     for (i, &item) in s_bytes.iter().enumerate() {
@@ -25,4 +30,16 @@ fn first_word(s: &String) -> usize {
     }
 
     s.len()
+}
+
+fn first_word_with_slice(s: &String) -> &str {
+    let s_bytes = s.as_bytes();
+
+    for (i, &item) in s_bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    &s[..]
 }
