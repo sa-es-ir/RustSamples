4d8ed27207e83f06eaca11b7a6f69ce74f998363
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-03-10 23:08:23 +0700

    feature: create and play with enums

--- enum_types/src/main.rs
+++ enum_types/src/main.rs
@@ -1,3 +1,34 @@
+use std::fmt::format;
+
 fn main() {
-    println!("Hello, world!");
+    let four = IpAddrKind::V4;
+
+    println!("Ip kind {:?}", four);
+
+    let four = IpKindWithValue::V6 { x: 123, y: 124 };
+
+    println!("Ip kind {:?}", four);
+
+    let four = IpKindWithValue::V4Numbers(127, 0, 0, 1);
+
+    println!("Ip kind {:?}", four.V4Numbers());
+}
+
+#[derive(Debug)]
+enum IpAddrKind {
+    V4,
+    V6,
+}
+
+#[derive(Debug)]
+enum IpKindWithValue {
+    V4(String),
+    V6 { x: u32, y: u32 },
+    V4Numbers(u32, u32, u32, u32),
+}
+
+impl IpKindWithValue {
+    fn V4Numbers(&self) {
+        println!("----- {:?}", self);
+    }
 }
