9c584462d5e0093241433a0b0d5ea51a0e12fad0
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-03-10 23:55:33 +0700

    feature: add match statement with enum

---
+++ match_details/src/main.rs
@@ -0,0 +1,38 @@
+fn main() {
+    let coin = Coin::Penny;
+
+    println!("coin in cents: {}", value_in_cents(coin));
+
+    let q_coin = Coin::Quarter(UsState::Alaska);
+    println!("coin in cents: {}", value_in_cents(q_coin));
+}
+
+#[derive(Debug)]
+enum UsState {
+    NY,
+    Alabama,
+    Alaska,
+    //others
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u32 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky 1 cent");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("us state {:?}", state);
+            25
+        }
+    }
+}
