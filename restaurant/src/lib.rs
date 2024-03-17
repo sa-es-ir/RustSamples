1366fe3e50676f49464251cf3dd5e78340e5135c
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-03-17 23:00:42 +0700

    feature: use super for access to parent module

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,6 +1,7 @@
 pub mod front_of_house {
     pub mod hosting {
         pub fn add_to_waitlist() {
+            super::super::deliver_order();
             println!("add_to_waitlist called");
         }
 
@@ -14,4 +15,15 @@ pub mod front_of_house {
 
         fn take_payment() {}
     }
+}
+
+fn deliver_order() {}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+        super::deliver_order();
+    }
+
+    fn cook_order() {}
 }
