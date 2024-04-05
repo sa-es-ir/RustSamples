03a85e83f30cbfe0b0a6425e9cea840881942115
Saeed Esmaeelinejad <s.esmaeelinejad@gmail.com>
2024-04-05 23:39:05 +0700

    playing with string

--- strings/src/main.rs
+++ strings/src/main.rs
@@ -1,3 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    let s = String::from("Hello");
+    let s2 = String::from("world!");
+
+    let s3_1 = format!("{s} {s2}");
+    let s3 = s + &s2;
+    println!("{} {}", s3_1, s2);
+
+    let mut string = String::from("Здравствуйте");
+
+    if string.contains("З") {
+        println!("YES");
+    }
+
+    for c in string.chars() {
+        print!("{c}-");
+    }
+    println!("");
+    string = string.replace("З", "S").replace("драв", "ZXC");
+    println!("-----------");
+    for c in string.chars() {
+        print!("{c}-");
+    }
 }
